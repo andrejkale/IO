cb03335fedfeff9d17d48c319de5ae963ad5f03a
Andrej <andrejkaletest@gmail.com>
2017-07-30 11:17:33 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.io.*;
+
+import static java.lang.System.out;
+
+public class Main {
+    public static final String sourceFileName = "sourse.txt";
+    public static final String destFileName = "dest.txt";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader resder = new BufferedReader(new FileReader(sourceFileName));
+        PrintWriter write = new PrintWriter(destFileName);
+        String s = null;
+        int words = 0, lines = 0, chars = 0;
+        while ((s = resder.readLine())!=null){
+            lines++;
+            String[] ws = s.trim().split("\\s");
+            words+=ws.length;
+            for (String l:ws
+                 ) {
+                chars+=l.length();
+                write.println(s.toUpperCase());
+
+            }
+
+        }
+        resder.close();
+        write.close();
+        out.printf("Скопировано символов - %d; слов-%d; строк-%d\n", chars,words,lines);
+    }
+}
